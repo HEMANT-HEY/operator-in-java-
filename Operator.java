109468f2a98e433f43555200440c967663287f05
HEMANT <100529584+HEMANT-HEY@users.noreply.github.com>
2022-09-04 13:29:52 +0530

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,12 @@
+impor java.util.Scanner;
+class {
+  
+
+public static void main{
+  Scanner sc=new Scanner(System.in);
+   int a= sc.nextInt();
+  int b=sc.nextInt();
+  int c=a+b;
+  System.out.println("sum is"+c);
+  }
+}
